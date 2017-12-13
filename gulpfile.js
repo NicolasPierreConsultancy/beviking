b03272000ef6105b633225cf21685886dfb0daa1
nicolaspierre1990 <nico_kestens@hotmail.com>
2017-12-13 14:44:57 +0100

    Styling

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,7 @@ var pkg = require('./package.json');
 
 // Set the banner content
 var banner = ['/*!\n',
-  ' * Nicolas Pierre Consultancy- <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+  ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
   ' * Copyright 2017-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
   ' * Licensed under <%= pkg.license %> (https://github.com/beviking/<%= pkg.name %>/blob/master/LICENSE)\n',
   ' */\n',
@@ -21,3 +21,36 @@ gulp.task('sass', function() {
       }))
       .pipe(gulp.dest('css'))
   });
+
+
+  gulp.task('copy', function() {
+    gulp.src([
+        'node_modules/bootstrap/dist/**/*',
+        '!**/npm.js',
+        '!**/bootstrap-theme.*',
+        '!**/*.map'
+      ])
+      .pipe(gulp.dest('vendor/bootstrap'))
+  
+    gulp.src(['node_modules/jquery/dist/jquery.js', 'node_modules/jquery/dist/jquery.min.js'])
+      .pipe(gulp.dest('vendor/jquery'))
+  
+    gulp.src(['node_modules/magnific-popup/dist/*'])
+      .pipe(gulp.dest('vendor/magnific-popup'))
+  
+    gulp.src(['node_modules/scrollreveal/dist/*.js'])
+      .pipe(gulp.dest('vendor/scrollreveal'))
+  
+    gulp.src(['node_modules/jquery.easing/*.js'])
+      .pipe(gulp.dest('vendor/jquery-easing'))
+  
+    gulp.src([
+        'node_modules/font-awesome/**',
+        '!node_modules/font-awesome/**/*.map',
+        '!node_modules/font-awesome/.npmignore',
+        '!node_modules/font-awesome/*.txt',
+        '!node_modules/font-awesome/*.md',
+        '!node_modules/font-awesome/*.json'
+      ])
+      .pipe(gulp.dest('vendor/font-awesome'))
+  })
