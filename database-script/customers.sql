dd43224b4ac46e13f07e21eecc71f948d4f4ea6d
nicolaspierre1990 <nico_kestens@hotmail.com>
2018-01-15 17:22:17 +0100

    Work done on services, need to work on calendar and gallery

--- database-script/customers.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Customers(
-    CustomerID int NOT NULL AUTO_INCREMENT,
-    FirstName VARCHAR(255) NOT NULL,
-    LastName VARCHAR(255) NOT NULL,
-    Email VARCHAR(255) NOT NULL,
-    PhoneNumber VARCHAR(255) NOT NULL,
-    NewsLetter BIT,
-    PRIMARY KEY(CustomerID)
-)
--- database-script/eventspaces.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE EventSpace(
-    EventSpaceID int NOT NULL AUTO_INCREMENT,
-    Event_Start TIMESTAMP,
-    Event_End TIMESTAMP,
-    PRIMARY KEY(EventSpaceID)    
-)
